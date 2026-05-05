a7bb9fe428e95f5e859bdd023856c819ddeb5eb1
Yridenkek <kugaevskiyi48@gmail.com>
2026-05-05 19:46:34 +0500

    UPD May Rates

--- database/db_manager.py
+++ database/db_manager.py
@@ -93,9 +93,7 @@ def init_rates_table_geely():
             rate60 REAL NOT NULL,
             rate72 REAL NOT NULL,
             rate84 REAL NOT NULL,
-            rate96 REAL NOT NULL,
-            rate108 REAL NOT NULL,
-            rate120 REAL NOT NULL
+            rate96 REAL NOT NULL
         )
     """)
     conn.commit()
@@ -121,10 +119,6 @@ def get_credit_rate_geely(pervak, term_months):
         column = "rate84"
     elif term_months == 96:
         column = "rate96"
-    elif term_months == 108:
-        column = "rate108"
-    elif term_months == 120:
-        column = "rate120"
     else:
         conn.close()
         return None
