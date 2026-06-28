3b9dd93fe4ca8594ed665004f3b9ad6c47174547
Yridenkek <kugaevskiyi48@gmail.com>
2026-06-28 13:30:10 +0500

    dop upd

--- pages/calc_internal.py
+++ pages/calc_internal.py
@@ -263,7 +263,7 @@ if st.session_state.show_col2 and col2 is not None:
                     else:
                         vozm += car_data['tradein']
                 
-                dopoborud = (order_price / 2) + pereliv
+                dopoborud = (order_price * 0,6) + pereliv
                 insurance_value = 80000 if is_credit else 0
                 tradein_value = 100000 if is_tradein else 0
                 
